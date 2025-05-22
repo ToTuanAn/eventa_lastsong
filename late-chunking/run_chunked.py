8f1208ac7782ec6372a8034f67bebc0c6d19c643
baoht <baoht@vng.com.vn>
2025-05-22 16:53:42 +0700

    [latechunking]

--- late-chunking/run_chunked.py
+++ late-chunking/run_chunked.py
@@ -15,12 +15,14 @@ from chunked_pooling import (
     chunk_by_config2,
 )
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # load model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained(
-    "jinaai/jina-embeddings-v2-base-en", trust_remote_code=True
+    "jinaai/jina-embeddings-v2-base-en", trust_remote_code=True, device_map=device
 )
 model = AutoModel.from_pretrained(
-    "jinaai/jina-embeddings-v2-base-en", trust_remote_code=True
+    "jinaai/jina-embeddings-v2-base-en", trust_remote_code=True, device_map=device
 )
 
 def process_str(s):
@@ -37,7 +39,7 @@ model.eval()
 db_name = "database_v4"
 ext = "parquet"
 
-data_df = pd.read_parquet(f"../processed_data/{db_name}.{ext}", engine="auto")
+data_df = pd.read_parquet(f"..\\processed_data\\{db_name}.{ext}", engine="auto")
 data_df = data_df.reset_index(drop=True)
 data_df["row_number"] = data_df.index
 data_df["chunk_emb"] = ["" for _ in range(len(data_df))]
@@ -62,6 +64,7 @@ for idx, row in tqdm(data_df.iterrows(), total=len(data_df)):
         )
 
         inputs = tokenizer(article_context, return_tensors="pt")
+        inputs = {k: v.to(device) for k, v in inputs.items()}
         with torch.no_grad():
             model_output = model(**inputs)
 
